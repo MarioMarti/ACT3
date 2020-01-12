6cc43b5eba4b1ee96f5fb0f8bf895082c81fb832
Mario <martisiso99@gmail.com>
2020-01-12 16:25:03 +0100

    HealthCardID + ProductID without test

--- ProductID.java
+++ ProductID.java
@@ -1,5 +1,26 @@
 package data;
 
-public class ProductID {
-    prueba;
+final public class ProductID {
+
+    private final String productID;
+
+    public ProductID (String code) { this.productID = code;}
+
+    public String getProductID() { return productID; }
+
+    @Override
+    public boolean equals (Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductID prodID = (ProductID) o;
+        return productID.equals(prodID.productID);
+    }
+
+    @Override
+    public int hashCode() {return productID.hashCode();}
+
+    @Override
+    public String toString() {
+        return "HealthCardID(" + "personal code )='" + productID + '\'' + '}';
+    }
 }
